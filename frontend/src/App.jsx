ce0bd7d802b900af12a7cefb3bb20702733fa89a
wdcc-csc <chenshucong8@gmail.com>
2025-04-25 18:57:23 +1000

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,6 @@ import { AuthProvider } from './contexts/AuthContext';
 import { LanguageProvider } from './contexts/LanguageContext';
 import Navbar from './component/Navbar';
 import ProtectedRoute from './component/ProtectedRoute';
-<<<<<<< HEAD
-=======
-
-
->>>>>>> bf39a08 (Initial commit)
 
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -19,7 +14,6 @@ import SessionAdmin from './pages/SessionAdmin';
 import JoinGame from './pages/JoinGame';
 import PlayerGame from './pages/PlayerGame';
 import PlayerResults from './pages/PlayerResults';
-
 
 const theme = createTheme({
   palette: {
@@ -44,46 +38,46 @@ function App() {
               {/* Public routes */}
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
-              
+
               {/* Player routes */}
               <Route path="/play" element={<JoinGame />} />
               <Route path="/play/:playerId" element={<PlayerGame />} />
               <Route path="/results/:playerId" element={<PlayerResults />} />
-              
+
               {/* Protected admin routes */}
-              <Route 
-                path="/dashboard" 
+              <Route
+                path="/dashboard"
                 element={
                   <ProtectedRoute>
                     <Dashboard />
                   </ProtectedRoute>
-                } 
+                }
               />
-              <Route 
-                path="/game/:gameId" 
+              <Route
+                path="/game/:gameId"
                 element={
                   <ProtectedRoute>
                     <GameEdit />
                   </ProtectedRoute>
-                } 
+                }
               />
-              <Route 
-                path="/game/:gameId/question/:questionId" 
+              <Route
+                path="/game/:gameId/question/:questionId"
                 element={
                   <ProtectedRoute>
                     <QuestionEdit />
                   </ProtectedRoute>
-                } 
+                }
               />
-              <Route 
-                path="/session/:sessionId" 
+              <Route
+                path="/session/:sessionId"
                 element={
                   <ProtectedRoute>
                     <SessionAdmin />
                   </ProtectedRoute>
-                } 
+                }
               />
-              
+
               {/* Default routes */}
               <Route path="/" element={<Navigate to="/login" replace />} />
               <Route path="*" element={<Navigate to="/login" replace />} />
